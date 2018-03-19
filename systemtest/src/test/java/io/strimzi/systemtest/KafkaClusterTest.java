be023e84dec5e7a41cc3045eeec40304134799ac
sromanov <BITsFYY894N>
2018-03-19 15:48:25 +0200

    1. Updated log message

--- systemtest/src/test/java/io/strimzi/systemtest/KafkaClusterTest.java
+++ systemtest/src/test/java/io/strimzi/systemtest/KafkaClusterTest.java
@@ -271,7 +271,7 @@ public class KafkaClusterTest {
         kubeClient.deleteByName(DEPLOYMENT, topicControllerName);
         kubeClient.waitForResourceDeletion(DEPLOYMENT, topicControllerName);
 
-        LOGGER.info("Waiting to recovery {}", topicControllerName);
+        LOGGER.info("Waiting for recovery {}", topicControllerName);
         kubeClient.waitForDeployment(topicControllerName);
     }
 
@@ -286,7 +286,7 @@ public class KafkaClusterTest {
         kubeClient.deleteByName(DEPLOYMENT, clusterControllerName);
         kubeClient.waitForResourceDeletion(DEPLOYMENT, clusterControllerName);
 
-        LOGGER.info("Waiting to recovery {}", clusterControllerName);
+        LOGGER.info("Waiting for recovery {}", clusterControllerName);
         kubeClient.waitForDeployment(clusterControllerName);
     }
 
@@ -302,7 +302,7 @@ public class KafkaClusterTest {
         kubeClient.deleteByName(STATEFUL_SET, kafkaStatefulSetName);
         kubeClient.waitForResourceDeletion(STATEFUL_SET, kafkaStatefulSetName);
 
-        LOGGER.info("Waiting to recovery {}", kafkaStatefulSetName);
+        LOGGER.info("Waiting for recovery {}", kafkaStatefulSetName);
         kubeClient.waitForStatefulSet(kafkaStatefulSetName, 1);
     }
 
@@ -318,7 +318,7 @@ public class KafkaClusterTest {
         kubeClient.deleteByName(STATEFUL_SET, zookeeperStatefulSetName);
         kubeClient.waitForResourceDeletion(STATEFUL_SET, zookeeperStatefulSetName);
 
-        LOGGER.info("Waiting to recovery {}", zookeeperStatefulSetName);
+        LOGGER.info("Waiting for recovery {}", zookeeperStatefulSetName);
         kubeClient.waitForStatefulSet(zookeeperStatefulSetName, 1);
     }
 }
